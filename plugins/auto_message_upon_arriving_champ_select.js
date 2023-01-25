80e205509ab3ccdc47eee3fd6eb7240c34db9c4c
teisseire117 <43145883+teisseire117@users.noreply.github.com>
2023-01-25 03:51:05 +0100

    Update auto_message_upon_arriving_champ_select.js
    Made it so it doesn't modify phase string to determine if message has already been sent

--- plugins/auto_message_upon_arriving_champ_select.js
+++ plugins/auto_message_upon_arriving_champ_select.js
@@ -4,6 +4,9 @@ let pvp_net_room_id; // chat room id
 let regex1 = /\/lol-chat\/v1\/conversations\/.+\/messages\/.+/
 let regex2 = /(?:\/lol-chat\/v1\/conversations\/)(.+)(?:\/messages\/.+)/
 let message_to_send = "https://twitch-tools.lolarchiver.com"
+let message_sent = false;  // determine whether or not the message has been sent in the lobby, so to not spam it
+let message_sent_phase; // stores previous phase state
+
 /**
  * Send message in the current lobby.
  * @param {message} endpoint Message to be send in lobby
@@ -26,8 +29,9 @@ let sendMessageUponArrivingLobby = async message => {
 	if (utils.debug_sub)
 		console.log(utils.phase, utils.pvp_net_id, utils.summoner_id, pvp_net_room_id)
 
-	if (regex1.exec(data[2]["uri"]) && data[2]["data"] && "body" in data[2]["data"] && data[2]["data"]["body"] == "joined_room" && phasesTracked.includes(utils.phase)) {
-		utils.phase = utils.phase + "AdvertiseDone"
+	if (!message_sent && regex1.exec(data[2]["uri"]) && data[2]["data"] && "body" in data[2]["data"] && data[2]["data"]["body"] == "joined_room" && phasesTracked.includes(utils.phase)) {
+		message_sent = true
+		message_sent_phase = utils.phase
 		pvp_net_room_id = regex2.exec(data[2]["uri"])[1]
 		clearTimeout(set_timeout_player_joined) // goal is to delay the sending of the message until last player joined. Remove this line if necessary
 		set_timeout_player_joined = setTimeout(async () => {
@@ -36,6 +40,10 @@ let sendMessageUponArrivingLobby = async message => {
 	}
 }
 
+/** If phase changes, we reset message_sent to false as we most likely changed lobby */
+let resetMessageSentStatus = async message => {if (JSON.parse(message["data"])[2]["data"] != message_sent_phase) message_sent = false}
+
 window.addEventListener('DOMContentLoaded', () => {
 	utils.subscribe_endpoint("/lol-chat/v1/conversations", sendMessageUponArrivingLobby)
+	utils.subscribe_endpoint('/lol-gameflow/v1/gameflow-phase', resetMessageSentStatus)
 })
